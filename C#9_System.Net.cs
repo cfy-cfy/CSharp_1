fef795889ec4b66d0e7945b745a5e95ef3745f1d
cfy-cfy <70810503+cfy-cfy@users.noreply.github.com>
2021-10-16 16:35:34 +0800

    Update C#9_System.Net.cs

--- C#9_System.Net.cs
+++ C#9_System.Net.cs
@@ -1,10 +1,13 @@
+//Rextester.Program.Main 是代码入口函数，不要改变它.
+//编译版本 4.0.30319.17929 for Microsoft (R) .NET Framework 4.5
+
 using System;
 using System.IO;
 using System.Web;
 using System.Net;
 using System.Text.RegularExpressions;
 
-namespace HelloWorld
+namespace Rextester
 {
 	public class Program
 	{
@@ -13,6 +16,7 @@ namespace HelloWorld
 			
 			//string url_path ="https://www.baidu.com/";
 			string url_path ="https://raw.githubusercontent.com/cfy-cfy/VBA_1/main/1_Download%20Llist.txt";
+			//string url_path ="https://raw.githubusercontent.com/hunkim/DeepLearningZeroToAll/master/data-03-diabetes.csv";
 			Uri file_uri = new Uri(url_path);
 			WebRequest request = WebRequest.Create(file_uri);  
 			WebResponse response = request.GetResponse(); 
