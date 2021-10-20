e8dc21cd943953c1c0f07b11796fa4b0cb0295c5
cfy-cfy <70810503+cfy-cfy@users.noreply.github.com>
2021-10-20 11:19:18 +0800

    Create C#10_json.cs

---
+++ C#10_json/C#10_json.cs
@@ -0,0 +1,78 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Net;
+using System.IO;
+
+namespace HelloWorld
+{
+    public static void Main(string[] args)
+    {
+      string url = "http://www.kuaidi100.com/query?type=shunfeng&postid=367847964498";
+      string getJson = HttpUitls.Get(url);
+      Console.WriteLine(getJson);
+    }
+    
+    public class HttpUitls
+    {
+        public static string Get(string Url)
+        {
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(Url);
+            request.Proxy = null;
+            request.KeepAlive = false;
+            request.Method = "GET";
+            request.ContentType = "application/json; charset=UTF-8";
+            request.AutomaticDecompression = DecompressionMethods.GZip;
+
+            HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+            Stream myResponseStream = response.GetResponseStream();
+            StreamReader myStreamReader = new StreamReader(myResponseStream, Encoding.UTF8);
+            string retString = myStreamReader.ReadToEnd();
+
+            myStreamReader.Close();
+            myResponseStream.Close();
+
+            if (response != null)
+            {
+                response.Close();
+            }
+            if (request != null)
+            {
+                request.Abort();
+            }
+
+            return retString;
+        }
+
+        public static string Post(string Url, string Data, string Referer)
+        {
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(Url);
+            request.Method = "POST";
+            request.Referer = Referer;
+            byte[] bytes = Encoding.UTF8.GetBytes(Data);
+            request.ContentType = "application/x-www-form-urlencoded";
+            request.ContentLength = bytes.Length;
+            Stream myResponseStream = request.GetRequestStream();
+            myResponseStream.Write(bytes, 0, bytes.Length);
+
+            HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+            StreamReader myStreamReader = new StreamReader(response.GetResponseStream(), Encoding.UTF8);
+            string retString = myStreamReader.ReadToEnd();
+
+            myStreamReader.Close();
+            myResponseStream.Close();
+
+            if (response != null)
+            {
+                response.Close();
+            }
+            if (request != null)
+            {
+                request.Abort();
+            }
+            return retString;
+        }
+    
+    }
+}
