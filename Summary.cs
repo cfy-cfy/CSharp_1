fd48c9fb09c44cfadb6ebe4d2ebb88fb3ff5c1b4
cfy-cfy <70810503+cfy-cfy@users.noreply.github.com>
2021-10-21 16:31:06 +0800

    Create Summary.cs

---
+++ Summary.cs
@@ -0,0 +1,18 @@
+【1 访问符】public、private、internal、protected、
+【2 部分类/部分方法】partial
+【3 修饰符】static、virtual、abstract、sealed、override、readonly、new
+【4 方法】void、Task、Task<TResult>
+【5 数据类型】sbyte、byte、int、uint、long、ulong、short、ushort
+             single、double、float、char
+             decimal、
+【6 集合】ArrayList、Stack、Queue、Hastable、SortedList
+【7 泛型集合】List<T>、Dictionary<K,V>
+【8 接口】interface
+【9 结构体】struct
+【10 枚举】enum
+【11 输出】return、out
+【12 异步】Thread、async、await、process
+【13 委托事件】delegate、event
+【14 匿名】lambda
+【15 操作excel】com——Microsoft.Office.Interop、 NIPO、MySQL、 Access 、 DataSet 、 DataTabel
+【16 特性】Attribute
