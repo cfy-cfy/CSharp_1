b05dd2bca4dcfa13603af8cdef093f6d21170a22
cfy-cfy <70810503+cfy-cfy@users.noreply.github.com>
2021-11-25 11:02:22 +0800

    Update C#8_LINQ.cs

--- C#8_System.IO+LINQ/C#8_LINQ.cs
+++ C#8_System.IO+LINQ/C#8_LINQ.cs
@@ -5,25 +5,37 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.IO;
+using HtmlAgilityPack;
+using System.Text.RegularExpressions;  // regexp
+using System.Drawing;
+using System.Data;
 
-namespace HelloWorld
+public class Program
 {
-  class IntroToLINQ
+  public static void Main(string[] args)
   {
-      static void Main()
-      {
-          // The Three Parts of a LINQ Query:
-          // 1. Data source.
-          int[] numbers = new int[7] { 0, 1, 2, 3, 4, 5, 6 };
+   
+    Linq.sub_Linq1();
 
-          // 2. Query creation.
-          // numQuery is an IEnumerable<int>
+  }
+
+}
+
+public class Linq
+{
+    public static void sub_Linq1()
+    {
+          int[] numbers = new int[7] { 0, 1, 2, 3, 4, 5, 6 };
+    
           var numQuery =
               from num in numbers
               where (num % 2) == 0
               select num;
 
-          // 3. Query execution.
           foreach (int num in numQuery)
           {
               Console.WriteLine("{0,1} ", num);
@@ -32,19 +44,24 @@ namespace HelloWorld
          int evenNumCount = numQuery.Count();
          Console.WriteLine("————————————————————————————————{0} ", evenNumCount);
 
-         // 4. 要强制立即执行任何查询并缓存其结果，可调用 ToList 或 ToArray 方法。
+         // 【要强制立即执行任何查询并缓存其结果，可调用 ToList 或 ToArray 方法】
+
+         // ————————————————————————————————————————————    【 ToList 】
           var arrList=numQuery.ToList();
           foreach (int arr in arrList)
           {
                Console.WriteLine(arr);
           }
           
+         // ————————————————————————————————————————————    【 ToList 】
+
           List<int> numQuery2 =
             (from num in numbers
             where (num % 2) == 0
             select num).ToList();
           Console.WriteLine($"————————————{numQuery2.Count}————————————————————");
 
+         // ————————————————————————————————————————————    【 ToArray 】
           var numQuery3 =
               (from num in numbers
               where (num % 2) == 0
@@ -53,9 +70,12 @@ namespace HelloWorld
           foreach (var arr in numQuery3)
           {
                Console.WriteLine(arr);
-          }
+          }  
 
-      }
-  }
+
+    }
+
 }
+
+
 
